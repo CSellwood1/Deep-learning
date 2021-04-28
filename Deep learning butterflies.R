d2dab3e39cb8a869b090ed0898c509abf0a6202f
Colleen Sellwood <C.Sellwood@newcastle.ac.uk>
2021-04-28 17:32:37 +0100

    script for project downloading butterfly images, and added .jpg to gitignore

--- Deep learning butterflies.R
+++ Deep learning butterflies.R
@@ -4,3 +4,35 @@ library(rinat)#for retrieving records
 library(sf)
 
 #use a script made by Dr Sanderson that makes a download images func
+source("download_images.R")
+#get simple GB map
+gb_ll <- readRDS("gb_simple.RDS")
+
+#lets get some brimstone butterfly images from inat (iNaturalist)
+#select only research quality, within GB
+brimstone_recs <-  get_inat_obs(taxon_name  = "Gonepteryx rhamni",
+                                bounds = gb_ll,
+                                quality = "research",
+                                # month=6,   # Month can be set.
+                                # year=2018, # Year can be set.
+                                maxresults = 600)
+#download the images
+download_images(spp_recs = brimstone_recs, spp_folder = "brimstone")
+#do the same for two more butterfly sp
+# Holly blue; Celastrina argiolus
+hollyblue_recs <-  get_inat_obs(taxon_name  = "Celastrina argiolus",
+                                bounds = gb_ll,
+                                quality = "research",
+                                maxresults = 600)
+
+
+# Orange tip; Anthocharis cardamines
+orangetip_recs <-  get_inat_obs(taxon_name  = "Anthocharis cardamines",
+                                bounds = gb_ll,
+                                quality = "research",
+                                maxresults = 600)
+download_images(spp_recs = hollyblue_recs, spp_folder = "hollyblue")
+download_images(spp_recs = orangetip_recs, spp_folder = "orangetip")
+
+#we want to separate the last 100 photos from each sp into folders for testing the model later
+image_files_path <- "images" # path to folder with photos
